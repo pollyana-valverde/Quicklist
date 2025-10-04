4e45f1cc2b9376cdd94112d852fad7575611d601
Pollyana Valverde <valverdepolly@gmail.com>
2025-10-04 15:27:03 -0300

    Função para excluir item finalizada + alert de exclusão

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,39 @@
 const addInput = document.querySelector("#add-input");
 const form = document.querySelector("#add-item form");
-const list = document.querySelector("#item-list ul")
+const list = document.querySelector("#item-list ul");
 
-// addInput.addEventListener("input", () => {
-//     console.log(addInput.value);
-// })
+const deleteAlert = document.querySelector("#delete-alert .container");
 
 form.onsubmit = (e) => {
     e.preventDefault()
-
     const newItem = document.createElement("li");
 
     newItem.classList.add("item", "px-base", "py-lg", "flex", "gap-1", "round-base");
 
     newItem.innerHTML = `
         <div class="checkbox-image"></div>
-        <input type="checkbox" name="item-row" id="item-row">
+        <input type="checkbox" name="item-row" id="${addInput.value}" value="${addInput.value}"></input> 
         <p>${addInput.value}</p>
-        <i class="trash-icon"></i>
+        <i class="trash-icon" onclick="deleteItem(this)"></i>
     `;
 
     list.append(newItem);
+
+    form.reset()
 }
 
+//função para remover o li
+function deleteItem(item) {
+    item.parentElement.remove()
+
+    const newAlert = document.createElement("div");
+    newAlert.classList.add("alert", "px-base", "py-base", "flex", "gap-1", "round-base");
+
+    newAlert.innerHTML = `
+       <img src="assets/icons/warning-circle-filled.svg" alt="">
+        <p>O item foi removido da lista</p>
+        <img src="assets/icons/delete-small.svg" onclick="this.parentElement.remove()" alt="">
+    `;
+
+    deleteAlert.append(newAlert)
+}
